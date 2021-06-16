6472ffce7618f7427cb431fff5c693670df66291
Guyzaidman <guyzaidm@gmail.com>
2021-06-16 10:38:08 +0300

    separated main page to columns

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="container">
     <h1 class="title">Main Page</h1>
-    <LoginPage v-if="!$root.store.username"></LoginPage>
-    <FavoriteGames v-else></FavoriteGames>
-    <LeagueInfo></LeagueInfo>
+    <b-container>
+      <b-row>
+        <b-col>
+          <LeagueInfo></LeagueInfo>
+        </b-col>
+        <b-col>
+          <LoginPage v-if="!$root.store.username"></LoginPage>
+          <FavoriteGames v-else></FavoriteGames>
+        </b-col>
+      </b-row>
+    </b-container>
+    
   </div>
 </template>
 
@@ -21,15 +30,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.RandomRecipes {
-  margin: 10px 0 10px;
-}
-.blur {
-  -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
-  filter: blur(2px);
-}
-::v-deep .blur .recipe-preview {
-  pointer-events: none;
-  cursor: default;
+.title{
+  text-align: center;
 }
 </style>
